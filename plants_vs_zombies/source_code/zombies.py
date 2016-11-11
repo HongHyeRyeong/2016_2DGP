abb2fa65f5a085cfbdc9242f8400f0a1b3719f0f
HongHyeRyeong <yjm12101210@naver.com>
2016-11-11 12:16:07 +0900

    Add files via upload

--- plants_vs_zombies/source_code/zombies.py
+++ plants_vs_zombies/source_code/zombies.py
@@ -9,6 +9,7 @@ class Zombie:
         self.dead_image = load_image('zom_die.png')
         self.x, self.y = 1400, (random.randint(0, 4)*100)+60
         self.walk_frame = random.randint(0, 5)
+        self.state = 0
 
     def update(self, frame_time):
         self.walk_frame = (self.walk_frame + 1) % 8
