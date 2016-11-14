959f6feb6b1f34af577fe5b26582a9c8fb18049b
HongHyeRyeong <yjm12101210@naver.com>
2016-11-15 03:19:15 +0900

    Add files via upload

--- plants_vs_zombies/source_code/main_state.py
+++ plants_vs_zombies/source_code/main_state.py
@@ -14,17 +14,19 @@ name = "MainState"
 
 #객체
 back = None
+font = None
 plants = None
 flowers = None
 walnuts = None
-bombs = None
 zombies = None
 attacks = None
+suns = None
 
 mousex, mousey = 0, 0
 selete_plant = 0
 space = [[0 for col in range(20)] for row in range(20)]
 zombie_cnt ,random_cnt = 0, 10
+sunpoint = 100
 
 class Background:
     def __init__(self):
@@ -37,30 +39,34 @@ class Background:
         self.selete.clip_draw((selete_plant - 1) * 100, 0, 100, 100, 1350, 545)
 
 def enter():
-    global back, plants, flowers, walnuts, bombs, zombies, attacks
+    global back, plants, flowers, walnuts, zombies, attacks, suns, font
+    global sunpoint
     back = Background()
+    font = load_font('ConsolaMalgun.ttf', 17)
     plants = []
     flowers = []
     walnuts = []
-    bombs = []
     zombies = []
     attacks = []
+    suns = []
 
 def exit():
-    global back, plants, flowers, walnuts, bombs, zombies, attacks
+    global back, plants, flowers, walnuts, zombies, attacks, suns
     global mousex, mousey
     global selete_plant, space
     global zombie_cnt, random_cnt
+    global sunpoint
     del(back)
     del(plants)
     del(flowers)
     del(walnuts)
-    del(bombs)
     del(zombies)
     del(attacks)
+    del(suns)
     mousex, mousey = 0, 0
     selete_plant = 0
     zombie_cnt, random_cnt = 0, 10
+    sunpoint = 0
     for i in range(20):
         for j in range(20):
             space[i][j] = 0
@@ -71,44 +77,39 @@ def pause():
 def resume():
     pass
 
+def selete_space():
+    global mousex, mousey, selete_plant
+    space[int(mousex / 100)][int(mousey / 100)] = 1
+    selete_plant = 0
+
 def set_object():
-    global plants, flowers, walnuts, bombs
+    global plants, flowers, walnuts, suns
     global mousex, mousey
     global selete_plant, space
+    global sunpoint
 
     if 260 < mousex < 980 and 90 < mousey < 600: # 땅
         if space[int(mousex / 100)][int(mousey / 100)] == 0:
-            if selete_plant == 1:
+            if selete_plant == 1 and sunpoint - 100 >= 0:
+                sunpoint -= 100
                 new_plant = Plant()
                 new_plant.creat(mousex, 599 - mousey)
                 plants.append(new_plant)
+                selete_space()
 
-                space[int(mousex / 100)][int(mousey / 100)] = 1
-                selete_plant = 0
-
-            elif selete_plant == 2:
+            elif selete_plant == 2 and sunpoint - 50 >= 0:
+                sunpoint -= 50
                 new_flower = Flower()
                 new_flower.creat(mousex, 599 - mousey)
                 flowers.append(new_flower)
+                selete_space()
 
-                space[int(mousex / 100)][int(mousey / 100)] = 1
-                selete_plant = 0
-
-            elif selete_plant == 3:
+            elif selete_plant == 3 and sunpoint - 50 >= 0:
+                sunpoint -= 50
                 new_walnut = Walnut()
                 new_walnut.creat(mousex, 599 - mousey)
                 walnuts.append(new_walnut)
-
-                space[int(mousex / 100)][int(mousey / 100)] = 1
-                selete_plant = 0
-
-            elif selete_plant == 4:
-                new_bomb = Bomb()
-                new_bomb.creat(mousex, 599 - mousey)
-                bombs.append(new_bomb)
-
-                space[int(mousex / 100)][int(mousey / 100)] = 1
-                selete_plant = 0
+                selete_space()
 
     elif 100 < mousex < 580 and 0 < mousey < 80: # 식물
         if 100 < mousex < 160:
@@ -117,28 +118,91 @@ def set_object():
             selete_plant = 2
         elif 220 < mousex < 280:
             selete_plant = 3
-        elif 280 < mousex < 340:
-            selete_plant = 4
-        elif 500 < mousex < 580: # 삽
-            selete_plant = 5
+        elif 500 < mousex < 575: # 삽
+            selete_plant = 0
+
+    elif 25 < mousex < 75 and 20 < mousey < 65: # 해
+        suncount = len(suns)
+        if suncount != 0:
+            sunpoint += suncount * 15
+            suns.clear()
+
 
 def creat():
-    global plants, zombies
+    global plants, zombies, attacks, suns, flowers
     global zombie_cnt, random_cnt
 
     zombie_cnt += 1
     if zombie_cnt == random_cnt:
         new_zombie = Zombie()
         zombies.append(new_zombie)
-        random_cnt = random.randint(10, 20)
+        random_cnt = random.randint(150, 200)
         zombie_cnt = 0
 
     for plant in plants:
-        if plant.attackcnt == 20:
+        if plant.attackcnt == 150:
             new_attack = Attack()
             new_attack.creat(plant.x, plant.y)
             attacks.append(new_attack)
             plant.attackcnt = 0
+
+    for flower in flowers:
+        if flower.suncnt == 300:
+            new_sun = Sun()
+            suns.append(new_sun)
+            flower.suncnt = 0
+
+def remove():
+    global attacks, zombies
+    for zombie in zombies:
+        if zombie.x < 0:
+            zombies.remove(zombie)
+        if zombie.state == zombie.END:
+            zombies.remove(zombie)
+
+    for attack in attacks:
+        if attack.x > 1400:
+            attacks.remove(attack)
+
+def collide(a, b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    left_b, bottom_b, right_b, top_b = b.get_bb()
+
+    if left_a > right_b:
+        return False
+    if right_a < left_b:
+        return False
+    if top_a < bottom_b:
+        return False
+    if bottom_a > top_b:
+        return False
+
+    return True
+
+def collide_check():
+    global plants, flowers, walnuts, zombies, attacks
+
+    for zombie in zombies:
+        for plant in plants:
+            if collide(zombie, plant):
+                plants.remove(plant)
+                zombie.state = zombie.ATTACK
+        for flower in flowers:
+            if collide(zombie, flower):
+                flowers.remove(flower)
+                zombie.state = zombie.ATTACK
+        for walnut in walnuts:
+            if collide(zombie, walnut):
+                if zombie.state == zombie.WALK:
+                    if walnut.life > 0:
+                        walnut.life -= 1
+                    else:
+                        walnuts.remove(walnut)
+                zombie.state = zombie.DIE
+        for attack in attacks:
+            if collide(attack, zombie):
+                zombie.state = zombie.DIE
+                attacks.remove(attack)
 
 def handle_events(frame_time):
     global mousex, mousey
@@ -156,20 +220,7 @@ def handle_events(frame_time):
             set_object()
 
 def update(frame_time):
-    global plants, flowers, walnuts, bombs, zombies, attacks
-
-    # 새로운 좀비, 식물
-    creat()
-
-    # 좀비 범위 넘어가면 삭제
-    for zombie in zombies:
-        if zombie.x < 0:
-            zombies.remove(zombie)
-
-    # 식물 공격 범위 넘어가면 삭제
-    for attack in attacks:
-        if attack.x > 1400:
-            attacks.remove(attack)
+    global plants, flowers, walnuts, zombies, attacks, suns
 
     if back.gamestart == True:
         for plant in plants:
@@ -178,29 +229,35 @@ def update(frame_time):
             attack.update(frame_time)
         for flower in flowers:
             flower.update(frame_time)
+        for sun in suns:
+            sun.update(frame_time)
         for walnut in walnuts:
             walnut.update(frame_time)
-        for bomb in bombs:
-            bomb.update(frame_time)
         for zombie in zombies:
             zombie.update(frame_time)
 
+    creat()
+    remove()
+    collide_check() # 충돌체크
+
 def draw(frame_time):
-    global plants, flowers, walnuts, bombs, zombies, attacks
+    global back, font
+    global plants, flowers, walnuts, zombies, attacks, suns
+    global sunpoint
     clear_canvas()
     if back.gamestart == True:
         back.draw()
+        font.draw(20, 504, '%d' % sunpoint)
         for plant in plants:
             plant.draw(frame_time)
         for flower in flowers:
             flower.draw(frame_time)
         for walnut in walnuts:
             walnut.draw(frame_time)
-        for bomb in bombs:
-            bomb.draw(frame_time)
         for zombie in zombies:
             zombie.draw(frame_time)
         for attack in attacks:
             attack.draw(frame_time)
+        for sun in suns:
+            sun.draw(frame_time)
     update_canvas()
-    delay(0.1)
--- plants_vs_zombies/source_code/zombies.py
+++ plants_vs_zombies/source_code/zombies.py
@@ -2,18 +2,57 @@ from pico2d import *
 import random
 
 class Zombie:
-    image = None
+    PIXEL_PER_METER = (10.0 / 0.5)           # 10 pixel 50 cm
+    RUN_SPEED_KMPH = 10.0                    # Km / Hour 속도
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+    TIME_PER_ACTION = 0.5
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 5
+
+    attack_time = 0.0
+
+    WALK, ATTACK, DIE, END = 1, 2, 3, 4
 
     def __init__(self):
         self.walk_image = load_image('zom_walk.png')
-        self.dead_image = load_image('zom_die.png')
+        self.attack_image = load_image('zom_attack.png')
+        self.die_image = load_image('zom_die.png')
         self.x, self.y = 1400, (random.randint(0, 4)*100)+60
-        self.walk_frame = random.randint(0, 5)
-        self.state = 0
+        self.total_frames = 0.0
+        self.walk_frame, self.attack_frame, self.die_frame = random.randint(0, 5), 0, 0
+        self.state = self.WALK
 
     def update(self, frame_time):
-        self.walk_frame = (self.walk_frame + 1) % 8
-        self.x -= 10
+        distance = self.RUN_SPEED_PPS * frame_time
+        self.total_frames += self.FRAMES_PER_ACTION * self.ACTION_PER_TIME * frame_time
+
+        if self.state == self.WALK:
+            self.walk_frame = int(self.total_frames) % 8
+            self.x -= distance
+        elif self.state == self.ATTACK:
+            self.attack_frame = int(self.total_frames) % 5
+            self.attack_time += frame_time
+            if int(self.attack_time) == 1:
+                self.attack_time = 0.0
+                self.state = self.WALK
+        elif self.state == self.DIE:
+            self.die_frame = int(self.total_frames) % 8
+            if self.die_frame == 7:
+                self.state = self.END
 
     def draw(self, frame_time):
-        self.walk_image.clip_draw(self.walk_frame*150, 0, 130, 130, self.x, self.y)
+        if self.state == self.WALK:
+            self.walk_image.clip_draw(self.walk_frame*150, 0, 150, 150, self.x, self.y)
+        if self.state == self.ATTACK:
+            self.attack_image.clip_draw(self.attack_frame*150, 0, 150, 150, self.x, self.y)
+        if self.state == self.DIE:
+            self.die_image.clip_draw(self.die_frame*200, 0, 200, 200, self.x, self.y)
+
+    def get_bb(self):
+        return self.x - 15, self.y - 55, self.x + 35, self.y + 25
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
